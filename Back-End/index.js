52446e7f860ef7cc8902275e0bae07c29e8adb22
AhmadSaleh99 <saleehsale816@gmail.com>
2024-04-21 03:33:51 -0500

    products

--- Back-End/index.js
+++ Back-End/index.js
@@ -3,6 +3,11 @@ const express = require("express");
 const dotenv = require("dotenv");
 const userRouter = require("./Routes/user.js");
 const authRouter = require("./Routes/auth.js");
+const productsRouter = require("./Routes/product.js");
+const cartRouter = require("./Routes/cart.js");
+const ordersRouter = require("./Routes/order.js");
+const stripeRouter = require("./Routes/stripe.js");
+const cors = require("cors");
 
 dotenv.config();
 // require("dotenv").config();
@@ -15,11 +20,17 @@ mongoose
   .catch((err) => console.log(err));
 
 ////////////////
+
+app.use(cors());
 app.use(express.json());
 // user Router
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/products", productsRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/orders", ordersRouter);
+app.use("/api/checkout", stripeRouter);
 
 app.listen(process.env.PORT || 5000, () =>
   console.log("listening on port 5000")
